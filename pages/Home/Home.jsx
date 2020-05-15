be63cc296da8e581a0cbbcf9136053dee009a525
Vincent Charpentier <animous.man.vc@gmail.com>
2020-05-15 21:42:30 +0200

    feat(home page): added basic home page

---
+++ pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Layout from 'Components/Layout';
+
+const HomePage = () => {
+  return (
+    <>
+      <Layout title="Home">Hello World</Layout>
+    </>
+  );
+};
+
+export default HomePage;
