5210c3a7d0940b8c4886b400478fe19a6c21220e
Vincent Charpentier <animous.man.vc@gmail.com>
2020-05-15 23:32:14 +0200

    feat(about): added about page

---
+++ pages/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Link from 'Components/Link';
+
+import styles from './About.module.scss';
+
+export default () => (
+  <div className={styles.root}>
+    <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" />
+    <p>This project is using TMDB - The movie database</p>
+    <Link href="/">Back to home page</Link>
+  </div>
+);
