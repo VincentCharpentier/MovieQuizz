cea709ab5cb104a3472ee130e285d339ef0c8785
Vincent Charpentier <animous.man.vc@gmail.com>
2020-05-15 22:06:32 +0200

    refactor(layout): remove page title mgmt

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import Head from 'next/head';
-// import '';
 import styles from './Layout.module.scss';
 
-const BASE_TITLE = 'Music Quiz';
-
-const computeTitle = (title) =>
-  title ? [BASE_TITLE, title].join(' - ') : BASE_TITLE;
-
-const Layout = ({ title = '', children }) => (
-  <>
-    <Head>
-      <title>{computeTitle(title)}</title>
-    </Head>
-    <div className={styles.root}>{children}</div>
-  </>
-);
+const Layout = ({ children }) => <div className={styles.root}>{children}</div>;
 
 export default Layout;
