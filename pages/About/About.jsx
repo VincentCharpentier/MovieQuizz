6e26e4012b7c92f54f763130a8de30e3103cf854
Vincent Charpentier <animous.man.vc@gmail.com>
2020-05-16 14:39:35 +0200

    feat(about): added sound links

--- pages/About/About.jsx
+++ pages/About/About.jsx
@@ -8,7 +8,14 @@ import styles from './About.module.scss';
 export default () => (
   <div className={styles.root}>
     <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" />
-    <p>This project is using TMDB - The movie database</p>
+    <p>Data: TMDB - The movie database</p>
+    <p>
+      Music: <a href="https://www.bensound.com">https://www.bensound.com</a>
+    </p>
+    <p>
+      Sound Effects: <a href="https://freesound.org/">https://freesound.org/</a>{' '}
+      (NenadSimic and broumbroum)
+    </p>
     <Link href={Routes.HOME}>Back to home page</Link>
   </div>
 );
