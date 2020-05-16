7775b5e20cde0cf04736bd4e21d955a586526492
Vincent Charpentier <animous.man.vc@gmail.com>
2020-05-16 11:24:20 +0200

    feat(layout): added background

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+
+import Background from 'Components/Background';
+
 import styles from './Layout.module.scss';
 
-const Layout = ({ children }) => <div className={styles.root}>{children}</div>;
+const Layout = ({ children }) => (
+  <div className={styles.root}>
+    {children}
+    <Background />
+  </div>
+);
 
 export default Layout;
