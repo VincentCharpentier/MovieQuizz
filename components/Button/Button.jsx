0f32fbad8d0c98a51fd3748f70e5dca2be7cdd8a
Vincent Charpentier <animous.man.vc@gmail.com>
2020-05-16 15:06:09 +0200

    feat(button): added support for click and hover sound

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,8 +1,9 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
 import useRandomColor from 'Hooks/useRandomColor';
+import { SFX_HOVER, SFX_SELECT } from 'Utils/sfx';
 
 import styles from './Button.module.scss';
 
@@ -10,16 +11,34 @@ const Button = ({
   color,
   tabIndex = 0,
   className = null,
+  onMouseEnter: _onMouseEnter = () => null,
+  onClick: _onClick = () => null,
+  clickSound,
   children,
   ...rest
 }) => {
+  const _clickSound = clickSound ?? SFX_SELECT;
   const randomColor = useRandomColor();
   let background = useMemo(() => color ?? randomColor, [color, randomColor]);
+
+  const onMouseEnter = useCallback((e) => {
+    SFX_HOVER.play();
+    _onMouseEnter(e);
+  }, []);
+  const onClick = useCallback(
+    (e) => {
+      _clickSound.play();
+      _onClick(e);
+    },
+    [_clickSound],
+  );
 
   const props = {
     style: { background },
     className: clsx(styles.root, className),
     tabIndex,
+    onMouseEnter,
+    onClick,
     ...rest,
   };
   return <button {...props}>{children}</button>;
