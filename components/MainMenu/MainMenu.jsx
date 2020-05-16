f468701055cb36fe7b986b9a59c2db71ae04c393
Vincent Charpentier <animous.man.vc@gmail.com>
2020-05-16 12:34:05 +0200

    feat(menu): added multi level menu

--- components/MainMenu/MainMenu.jsx
+++ components/MainMenu/MainMenu.jsx
@@ -1,15 +1,73 @@
-import React from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import clsx from 'clsx';
 
 import Button from 'Components/Button';
 
 import styles from './MainMenu.module.scss';
 
-const MainMenu = ({ className }) => (
-  <div className={clsx(styles.root, className)}>
-    <Button>Play !</Button>
-    <Button>Multiplayer</Button>
-  </div>
-);
+const MENU = {
+  MAIN: 'MAIN',
+  SETTINGS: 'SETTING',
+  SOUND: 'SOUND',
+};
+
+const PARENT_MENU = {
+  [MENU.SETTINGS]: MENU.MAIN,
+  [MENU.SOUND]: MENU.SETTINGS,
+};
+
+const isParentMenu = (menu, parentMenu) => {
+  let result = false;
+  const actualParent = PARENT_MENU[menu];
+  if (actualParent) {
+    if (actualParent === parentMenu) {
+      result = true;
+    } else {
+      result = isParentMenu(actualParent, parentMenu);
+    }
+  }
+  return result;
+};
+
+const Menu = ({ menuId, currentMenu, children }) => {
+  const isParent = useMemo(() => isParentMenu(currentMenu, menuId), [
+    menuId,
+    currentMenu,
+  ]);
+  const isCurrent = menuId === currentMenu;
+
+  const classes = clsx(
+    styles.menu,
+    isParent && styles.left,
+    isCurrent && styles.current,
+  );
+  return <div className={classes}>{children}</div>;
+};
+
+const MainMenu = ({ className }) => {
+  const [currentMenu, setCurrentMenu] = useState(MENU.MAIN);
+
+  const goto = useCallback((menuId) => () => setCurrentMenu(menuId), [
+    setCurrentMenu,
+  ]);
+
+  return (
+    <div className={clsx(styles.root, className)}>
+      <Menu menuId={MENU.MAIN} currentMenu={currentMenu}>
+        <Button>Play !</Button>
+        <Button>Multiplayer</Button>
+        <Button onClick={goto(MENU.SETTINGS)}>Settings</Button>
+      </Menu>
+      <Menu menuId={MENU.SETTINGS} currentMenu={currentMenu}>
+        <Button onClick={goto(MENU.SOUND)}>Sound</Button>
+        <Button onClick={goto(MENU.MAIN)}>Back</Button>
+      </Menu>
+      <Menu menuId={MENU.SOUND} currentMenu={currentMenu}>
+        <p>Nothing here yet !</p>
+        <Button onClick={goto(MENU.SETTINGS)}>Back</Button>
+      </Menu>
+    </div>
+  );
+};
 
 export default MainMenu;
