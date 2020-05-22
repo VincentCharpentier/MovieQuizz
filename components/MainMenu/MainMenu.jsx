26f64b319b767df76bf10ec4d7f0a95acb980e28
Vincent Charpentier <animous.man.vc@gmail.com>
2020-05-22 15:32:18 +0200

    feat(menu): added settings for sound

--- components/MainMenu/MainMenu.jsx
+++ components/MainMenu/MainMenu.jsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useState, useMemo } from 'react';
+import React, { useCallback, useContext, useState, useMemo } from 'react';
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
 
 import Button from 'Components/Button';
 import Routes from 'Constants/routes';
+import SettingsContext from 'Contexts/Settings';
 import { SFX_VALIDATE } from 'Utils/sfx';
 
 import styles from './MainMenu.module.scss';
@@ -49,30 +50,71 @@ const Menu = ({ menuId, currentMenu, children }) => {
 
 const MainMenu = ({ className }) => {
   const router = useRouter();
+  const {
+    soundActive,
+    soundVolume,
+    setSoundActive,
+    setSoundVolume,
+  } = useContext(SettingsContext);
 
-  const [currentMenu, setCurrentMenu] = useState(MENU.MAIN);
+  const [currentMenu, setCurrentMenu] = useState(MENU.SOUND);
 
   const goto = useCallback((menuId) => () => setCurrentMenu(menuId), [
     setCurrentMenu,
   ]);
   const navTo = useCallback((route) => () => router.push(route));
 
+  const toggleSound = useCallback(() => {
+    setSoundActive(!soundActive);
+  }, [setSoundActive, soundActive]);
+
+  const handleVolumeChange = useCallback(
+    (e) => setSoundVolume(e.target.value),
+    [setSoundVolume],
+  );
+
   return (
     <div className={clsx(styles.root, className)}>
       <Menu menuId={MENU.MAIN} currentMenu={currentMenu}>
-        <Button onClick={navTo(Routes.SOLO_GAME)} clickSound={SFX_VALIDATE}>
+        <Button
+          className={styles.menuEntry}
+          onClick={navTo(Routes.SOLO_GAME)}
+          clickSound={SFX_VALIDATE}>
           Play !
         </Button>
-        <Button disabled>Multiplayer</Button>
-        <Button onClick={goto(MENU.SETTINGS)}>Settings</Button>
+        <Button className={styles.menuEntry} disabled>
+          Multiplayer
+        </Button>
+        <Button className={styles.menuEntry} onClick={goto(MENU.SETTINGS)}>
+          Settings
+        </Button>
       </Menu>
       <Menu menuId={MENU.SETTINGS} currentMenu={currentMenu}>
-        <Button onClick={goto(MENU.SOUND)}>Sound</Button>
-        <Button onClick={goto(MENU.MAIN)}>Back</Button>
+        <Button className={styles.menuEntry} onClick={goto(MENU.SOUND)}>
+          Sound
+        </Button>
+        <Button className={styles.menuEntry} onClick={goto(MENU.MAIN)}>
+          Back
+        </Button>
       </Menu>
       <Menu menuId={MENU.SOUND} currentMenu={currentMenu}>
-        <p>Nothing here yet !</p>
-        <Button onClick={goto(MENU.SETTINGS)}>Back</Button>
+        <Button className={styles.menuEntry} onClick={toggleSound}>
+          Sound: {soundActive ? 'ON' : 'OFF'}
+        </Button>
+        <div className={clsx(styles.menuEntry, styles.volumeCtrl)}>
+          <p>Volume {soundVolume * 100}%</p>
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step=".1"
+            value={soundVolume}
+            onChange={handleVolumeChange}
+          />
+        </div>
+        <Button className={styles.menuEntry} onClick={goto(MENU.SETTINGS)}>
+          Back
+        </Button>
       </Menu>
     </div>
   );
